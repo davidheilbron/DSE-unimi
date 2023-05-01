b4d79fe502802a7f939a3aaf2f2294a75b981635
David Heilbron <david.heilbron@hotmail.com>
2023-05-01 12:06:45 +0200

    Update

--- Bayesian Statistics/Greenhouse gas emissions/VARX.R
+++ Bayesian Statistics/Greenhouse gas emissions/VARX.R
@@ -15,6 +15,8 @@ library(forecast)
 library(bvartools)
 library(coda)
 library(Matrix)
+library(MCMCpack)
+library(reshape2)
 
 #Load data
 bayes_data <- read.csv("bayes_data.csv")
@@ -36,7 +38,7 @@ Y  <- as.matrix(Yraw[5:nrow(Yraw),1])
 
 T <- nrow(Y) #number of observations
 n <- ncol(Y) #number of endogenous features
-m <- 5 #number of exogenous features
+m <- 3 #number of exogenous features
 y <- as.vector(t(Y)) #transform the features matrix into a single vector
 
 k <- n*p + m*p + 1 # number of coefficients in each equation
@@ -70,44 +72,80 @@ SURform2 <- function(X, n) {
   return(X_out)
 }
 
-X <- SURform2(X_tilde,n)
-copy_X <- X
-X <- as.matrix(X)
+#X <- SURform2(X_tilde,n)
+#X <- kronecker(diag(nrow(X_tilde)), X_tilde)
+X <- X_tilde
 
-rank(qr(X))
 qr(X)
 
 # Storage of variables
-store_Sig  <- array(0, dim=c(nsim,(n+m),(n+m)))
-store_beta <- array(0, dim=c(nsim,k*n))
+store_Sig  <- array(0, dim = c(nsim,(n + m),(n + m)))
+store_beta <- array(0, dim = c(nsim,k*n))
 store_yIR  <- matrix(0, nrow = n_hz, ncol = (n + m))
 
 # Initialize the chain 
 beta <- solve(t(X) %*% X) %*% t(X) %*% y
-e    <- matrix(y - X %*% beta, (n+m), T)
-Sig  <- t(e) %*% e/T
+e <- (y - X %*% beta)
+Sig <- t(e) %*% e/T
 iSig <- solve(Sig)
+isig <- iSig[1,1]
+
+construct_IR <- function(beta, Sig, n_hz, shock) {
+  n <- nrow(Sig)
+  p <- (nrow(beta) / n - 1) / n
+  CSig <- chol(Sig, lower = TRUE)
+  tmpZ1 <- matrix(0, ncol = n, nrow = p)
+  tmpZ <- matrix(0, ncol = n, nrow = p)
+  Yt1 <- CSig %*% shock
+  Yt <- rep(0, n)
+  yIR <- matrix(0, ncol = n_hz, nrow = n)
+  yIR[1,] <- t(Yt1)
+  
+  for (t in 2:n_hz) {
+    # update the regressors
+    tmpZ <- rbind(Yt, tmpZ[1:(p-1),])
+    tmpZ1 <- rbind(Yt1, tmpZ1[1:(p-1),])
+    
+    # evolution of variables if a shock hits
+    e <- CSig %*% rnorm(n)
+    Z1 <- as.vector(t(tmpZ1))
+    Xt1 <- kronecker(diag(n), cbind(1, Z1))
+    Yt1 <- Xt1 %*% beta + e
+    
+    # evolution of variables if no shocks hit
+    Z <- as.vector(t(tmpZ))
+    Xt <- kronecker(diag(n), cbind(1, Z))
+    Yt <- Xt %*% beta + e
+    
+    # the IR is the difference of the two scenarios
+    yIR[t,] <- t(Yt1 - Yt)
+  }
+  return(yIR)
+}
+
+
 
 ## Gibbs sampler 
 tic <- Sys.time()
 for (isim in 1:(nsim + burnin)) {
   # Posterior of beta \sim Normal
-  XiSig    <- t(X) %*% kronecker(diag(T),iSig)
-  XiSigX   <- XiSig %*% X
+  XiSig    <-  t(X) * isig
+  XiSigX   <- (t(X) %*% X) * isig
   Kbeta    <- iVbeta + XiSigX # posterior variance
   beta_hat <- solve(Kbeta) %*% (iVbeta %*% beta0 + XiSig %*% y) # posterior mean
   beta     <- beta_hat + chol2inv(chol(Kbeta)) %*% rnorm(n*k,0,1)
   
   # posterior of Sigma \sim iWishart
-  e    <- matrix(y - X %*% beta, n, T)    
-  Sig  <- riwish(nu0 + T, S0 + t(e) %*% e)
+  e    <- matrix(y - X %*% beta)    
+  #Sig  <- riwish(nu0 + T, S0 + t(e) %*% e) 
+  Sig  <- riwish(nu0 + T, S0)
   iSig <- solve(Sig)
   
   # Count the number of iterations
-  if (isim %% 1000 == 0) {
-    elapsed_time <- Sys.time() - start_time
-    print(paste("Iteration:", isim, "of", nsim + burnin, ". Elapsed time is", elapsed_time, "seconds."))
-  }
+  #if (isim %% 1000 == 0) {
+  #  elapsed_time <- Sys.time() - start_time
+  #  print(paste("Iteration:", isim, "of", nsim + burnin, ". Elapsed time is", elapsed_time, "seconds."))
+  #}
   
   #Store the parameters
   if (isim > burnin) {
