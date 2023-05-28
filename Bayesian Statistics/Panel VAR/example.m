ff994f78c036135b26b52ab61573e8d778da41ad
David Heilbron <david.heilbron@hotmail.com>
2023-05-28 18:49:44 +0200

    Update

--- Bayesian Statistics/Panel VAR/example.m
+++ Bayesian Statistics/Panel VAR/example.m
@@ -1,4 +1,13 @@
-load Data.mat
-% load spreadsheets
-% y = xlsread( filename%,%sheetname%) 
+%% Panel BVAR
+close all; clc; clear;
+
+cd 'C:\Users\Administrator\Documents\MATLAB\Panel BVAR';
+pvardata = readtable('pvar_data.xslx');
+
+startTime = datetime('2018-01-01');  % Specify the start time
+endTime = datetime('2020-01-01');    % Specify the end time
+
+% In-sample data
+y = pvardata(pvardata.Time >= startTime & pvardata.Time <= endTime,:);
+
 
